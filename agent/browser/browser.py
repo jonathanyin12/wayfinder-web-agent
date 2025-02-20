8dcf2fd9116b106da7c836e6220d4a1f0ebaa16b
Jonathan <jonathanyin.12@gmail.com>
2025-02-19 20:55:53 -0500

    add default browser size

--- agent/browser/browser.py
+++ agent/browser/browser.py
@@ -62,6 +62,7 @@ class AgentBrowser:
         user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
         self.context = await self.browser.new_context(
             user_agent=user_agent,
+            viewport={"width": 1280, "height": 1000},
         )
         self.page = await self.context.new_page()
         await self.go_to(url)
