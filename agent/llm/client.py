69260e0416745ef5a80b2ccd1e11fd74fdcf0b01
Jonathan <jonathanyin.12@gmail.com>
2025-04-19 16:49:38 -0400

    add support for more models

--- agent/llm/client.py
+++ agent/llm/client.py
@@ -23,9 +23,21 @@ PRICING = {
         "prompt_tokens": 2.5 / 1000000,
         "completion_tokens": 10 / 1000000,
     },
+    "gpt-4.1": {
+        "prompt_tokens": 2 / 1000000,
+        "completion_tokens": 8 / 1000000,
+    },
     "o1": {
         "prompt_tokens": 15 / 1000000,
         "completion_tokens": 60 / 1000000,
+    },
+    "o4-mini": {
+        "prompt_tokens": 1.1 / 1000000,
+        "completion_tokens": 4.4 / 1000000,
+    },
+    "o3": {
+        "prompt_tokens": 10 / 1000000,
+        "completion_tokens": 40 / 1000000,
     },
 }
 
